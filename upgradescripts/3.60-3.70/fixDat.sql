eecc714d6281ddb302025877327c94ffdd06f634
kovacm <michal@logici.cz>
2016-07-31 21:03:12 +0200

    Update plugin versions and sql script for database fix

---
+++ upgradescripts/3.60-3.70/fixDat.sql
@@ -0,0 +1,11 @@
+USE [NopCommerce]
+GO
+
+/****** Object:  Index [_dta_index_Category_6_375672386__K8_K1_K13_K14_K22_K23_K15_2_3_4_5_6_9_10_11_12_16_17_18_19_20_21]    Script Date: 7/31/2016 4:34:30 PM ******/
+DROP INDEX [_dta_index_Category_6_375672386__K8_K1_K13_K14_K22_K23_K15_2_3_4_5_6_9_10_11_12_16_17_18_19_20_21] ON [dbo].[Category]
+GO
+/****** Object:  Statistic [_dta_stat_375672386_21_1_14_13]    Script Date: 7/31/2016 4:35:15 PM ******/
+DROP STATISTICS [dbo].[Category].[_dta_stat_375672386_21_1_14_13]
+GO
+
+
