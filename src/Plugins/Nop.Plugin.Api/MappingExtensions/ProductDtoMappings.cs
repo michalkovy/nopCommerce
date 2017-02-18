5a5ddc4b8912cd81edf4666df61ee33aeab1a556
kovacm <michal@logici.cz>
2017-02-18 13:00:31 +0100

    Added Nop.Plugin.Api

---
+++ src/Plugins/Nop.Plugin.Api/MappingExtensions/ProductDtoMappings.cs
@@ -0,0 +1,14 @@
+﻿using Nop.Plugin.Api.AutoMapper;
+using Nop.Core.Domain.Catalog;
+using Nop.Plugin.Api.DTOs.Products;
+
+namespace Nop.Plugin.Api.MappingExtensions
+{
+    public static class ProductDtoMappings
+    {
+        public static ProductDto ToDto(this Product product)
+        {
+            return product.MapTo<Product, ProductDto>();
+        }
+    }
+}
